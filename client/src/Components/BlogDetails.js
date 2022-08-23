074fbecc427300b755f07e4cd845f063331b966d
Shivam Sharma <shivam.sharma@bijak.in>
2022-08-23 19:28:07 +0530

    code format and other minor change

--- client/src/Components/BlogDetails.js
+++ client/src/Components/BlogDetails.js
@@ -1,33 +1,35 @@
-import React from 'react'
+import React from 'react';
+import HomeSvg from '../assets/home.svg';
 
 function BlogDetails({ details, type, backtoHome }) {
-    return (
-        <div className="details">
-            {
-                type === 'new' ?
-                    <div className="newpost">
-                        <div>
-                        <nav>
-                        <button onClick={() => backtoHome()}><img src="../assets/home.svg"/></button>
-                        </nav>
-</div>
-<div className="editor">
-    <div className="editingtools"></div>
-                    <textarea></textarea>
-                    </div>
-                    </div>
-                    :
-                    <>
-                        <div className='blog-heading'>
-                            <button onClick={() => backtoHome()}><img src="../assets/home.svg"/></button>
-                            <h3>{details.title}</h3>
-                        </div>
-                        <div className="blog-content">
-                            {details.content}
-                        </div>
-                    </>
-            }
+  return (
+    <div className="details">
+      {type === 'new' ? (
+        <div className="newpost">
+          <div>
+            <nav>
+              <button onClick={() => backtoHome()}>
+                <img src={HomeSvg} alt="home" />
+              </button>
+            </nav>
+          </div>
+          <div className="editor">
+            <div className="editingtools"></div>
+            <textarea></textarea>
+          </div>
         </div>
-    )
+      ) : (
+        <>
+          <div className="blog-heading">
+            <button onClick={() => backtoHome()}>
+              <img src={HomeSvg} alt="home" />
+            </button>
+            <h3>{details.title}</h3>
+          </div>
+          <div className="blog-content">{details.content}</div>
+        </>
+      )}
+    </div>
+  );
 }
 export default BlogDetails;
